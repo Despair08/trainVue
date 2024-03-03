6ebbbc0b10dda2bda831d498185db09da0da8794
Despair08 <vodanila2008@ya.ru>
2024-03-03 09:40:41 +0300

    init:create app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="app">
-    <PostForm></PostForm>
-    <PostList :posts="posts"></PostList>
-
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Название">
+      <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Описание">
+      <button class="button" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>Название: </strong>{{post.title}}</div>
+      <div><strong>Описание: </strong>{{post.body}}</div>
+    </div>
   </div>
 </template>
+
 <script>
-import PostForm from '@/components/PostForm/PostForm.vue';
-import PostList from '@/components/PostList/PostList.vue';
 export default {
-  components: {
-    PostList,
-    PostForm,
-  },
-  data() {
-    return {
-      posts: [
-        { id: 1, title: 'JavaScript', body: 'Here are body of post' },
-        { id: 2, title: 'JavaScript 2', body: 'Here are body of post 2' },
-        { id: 3, title: 'JavaScript 3', body: 'Here are body of post 3' },
+  data(){
+    return{
+      posts:[
+        {id:1, title:'JavaScript 1',body:'JS Универсальный язык программирования 1'},
+        {id:2, title:'JavaScript 2',body:'JS Универсальный язык программирования 2'},
+        {id:3, title:'JavaScript 3',body:'JS Универсальный язык программирования 3'},
+        {id:4, title:'JavaScript 4',body:'JS Универсальный язык программирования 4'},
       ],
-      title: '',
-      body: '',
+        title:'',
+      body:'',
+
     }
+
   },
-  methods: {
-    createPost() {
+  methods:{
+    createPost(){
       const newPost = {
         id: Date.now(),
         title: this.title,
@@ -38,6 +53,8 @@ export default {
   }
 }
 </script>
+
 <style lang="scss">
-@import '@/scss/abstract/normalize.scss';
+@import '@/scss/abstract/normalize';
+@import '@/scss/components/App';
 </style>
